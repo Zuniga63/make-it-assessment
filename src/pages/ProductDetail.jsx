f177caafcd508e853743ada4248288d2805ab8fc
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-05-26 13:31:03 -0500

    add principal pages components

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class ProductDetail extends Component {
+  render() {
+    return <div>ProductDetail</div>;
+  }
+}
+
+export default ProductDetail;
