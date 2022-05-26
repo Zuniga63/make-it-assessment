57eeb1e6e3b36d4316f54c1c00b9372e1d9f5759
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-05-26 13:23:03 -0500

    add eslint, prettier and husky config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+const RULES = {
+  OFF: 'off',
+  ERROR: 'error',
+  WARN: 'warn',
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': RULES.ERROR,
+    'arrow-body-style': RULES.OFF,
+    'react/prop-types': RULES.OFF,
+  },
+};
